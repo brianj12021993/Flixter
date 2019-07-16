2c821bf2debb17701d191a329d5e9314472b530c
Brian Johnson <brianjohnsona@yahoo.com>
2019-07-16 01:55:56 +0000

    API Keys

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,24 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_authorized_for_current_lessons, only: [:show]
+
+	def create
+		@lessons = current_lesson.section.course(course params)
+			if @lesson.valid?
+				redirect_to_instructor_courses_path(@lessons)
+	end
+
 	def show
+		@lesson = Lessons.find(params[:id])
 	end
 
 	private
+
+  	def require_authorized_for_current_lesson
+    	if current_lesson.user != current_user
+      		redirect_to_course_sections, alert: 'Error Message Here'
+    	end
+  	end
 
   	helper_method :current_lesson
   	def current_lesson
