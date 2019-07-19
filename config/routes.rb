0d28ca5af45bb30271aa72112338e08c9a6d331c
Brian Johnson <brianjohnsona@yahoo.com>
2019-07-19 00:28:21 +0000

    formed the modal and cleaned code

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,7 @@ Rails.application.routes.draw do
 		resources :sections, only: [:update] do
       		resources :lessons, only: [:new, :create]
     	 end
-    	 resources :courses, only: [:new, :create, :show] do
-      		resources :sections, only: [:new, :create]
+    	 resources :courses, only: [:new, :create, :show] do      		
     	end
       end
 	end
