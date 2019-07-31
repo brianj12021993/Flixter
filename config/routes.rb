bcd59e790272edc055fd31ea62c024d02f6d5c50
Brian Johnson <brianjohnsona@yahoo.com>
2019-07-31 23:41:35 +0000

    fixed code

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
 		  resources :sections, only: [:update] do
         resources :lessons, only: [:new, :create]
       end
-        resources :courses, only: [:new, :create, :show] do      		
+      resources :courses, only: [:new, :create, :show] do 
+        resources :sections, only: [:create]     		
     	end
     end
 	end
